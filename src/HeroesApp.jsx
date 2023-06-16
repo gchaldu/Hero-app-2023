a2d17786334023f466a78cd4540c2aa6bb54b581
Gabriel Chaldu <gabrielchaldu@gmail.com>
2023-06-16 12:13:19 -0300

    version con script y otros

---
+++ src/HeroesApp.jsx
@@ -0,0 +1,11 @@
+import { Header } from './heroes/components/Header';
+import { AppRouter } from './router/AppRouter';
+
+
+export const HeroesApp = () => {
+  return (
+    <>
+        <AppRouter />
+    </>
+  )
+}
